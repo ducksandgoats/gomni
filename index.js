f47743f570b65e982ca7b077ea21867e6992a05c
resession <plainstud@gmail.com>
2022-04-23 22:54:13 -0500

    make change

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-function iden(){
-    return Date.now() + Math.random().toString(36).substring(2)
+class Gomni {
+    constructor(){}
+    iden(){
+        return Date.now() + Math.random().toString(36).substring(2)
+    }
+    toData(data){
+        return {[this.iden()]: data}
+    }
+    arrToObj(arr){
+        return arr.reduce((acc, item) => {return {...acc, [item.id]: item}}, {})
+    }      
+    objToArr(indexedObj){
+    return Object.values(indexedObj);
+    }
 }
 
+module.exports = Gomni
